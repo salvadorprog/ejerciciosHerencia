1b2616e2651f9d07a9f03ad05068d9698594b45c
Tequinox <txaxati2018@gmail.com>
2021-02-26 21:44:13 +0200

    Redefinido main, queda terminar opciones y corregir fallo al seleccionar barco

--- src/Barco/Main.java
+++ src/Barco/Main.java
@@ -4,65 +4,111 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Option;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// Antes de asignar barcos los creamos, por lo que creamos uno de cada como ejemplo
 		
-		Barco velero = new Velero("4190VGH", 47.3, 1978, 12);
-		Barco embarcacion = new EmbarcacionDeportiva("5670GHJ", 62.2, 2003, 570);
-		Barco yate = new YateDeLujo("7854XYZ", 890.9, 2021, 890, 230);
+		// Variables y entradas
 		
-		// Ahora generamos algunos alquileres
+		StringBuilder opcion = new StringBuilder();
+		Scanner entradaUsuario = new Scanner(System.in);
 		
-		Alquiler alq0 = new Alquiler("Hercules", "8912309218f", LocalDate.of(2021, 5, 10), 19, velero);			// --> En estos casos no especificamos la fecha de inicio 
-		Alquiler alq1 = new Alquiler("Javier", "874239dsdf", LocalDate.of(2021, 5, 13), 89, yate);					// por lo que captura la fecha actual del sistema <--
-		Alquiler alq2 = new Alquiler("María", "8432948g", LocalDate.of(2021, 10, 8) ,LocalDate.of(2021, 12, 10), 179, embarcacion);			// A este si le pasamos la fecha de inicio
+		StringBuilder matricula; 
+		Double eslora;
+		int anoFabricacion, cv, numeroCamarotes, numeroMastiles;
 		
-		// Obtener información de los alquileres
-		boolean exit = false;
-		Scanner entrada = new Scanner(System.in);
-		StringBuilder option = new StringBuilder();
-		while (!exit) {
+		// Creamos el barco
+		
+		Barco barco1;
+		
+		// Creamos el alquiler
+		
+		Alquiler alquiler1;
+		
+		// Programa
+		
+		while(!opcion.equals("exit")) {
 			System.out.println(
-					"\n 1. Obtener información del alquiler 1" +
-					"\n 2. Obtener precio del alquiler 1" +
-					"\n 3. Obtener información del alquiler 2" +
-					"\n 4. Obtener precio del alquiler 2" + 
-					"\n 5. Obtener información del alquiler 3" +
-					"\n 6. Obtener precio del alquiler 3"
-			);
-			System.out.print("Indique selección >>> ");
-			option = new StringBuilder(entrada.nextLine());
-			switch (Integer.parseInt(option.toString())) {
-				case 1: {
-					System.out.println(alq0.toString());
-					break;
+					"\n 1.Definir barco" +
+					"\n 3.Definir alquiler" +
+					"\n 4.Obtener información del barco" +
+					"\n 5.Obtener información del alquiler" + 
+					"\n 6.Obtener precio del barco" +
+					"\n 7.Obtener precio del alquiler"
+					);
+			System.out.print("Seleccione una función >> ");
+			try {
+				opcion = new StringBuilder(entradaUsuario.nextLine());
+			
+				switch (opcion.toString()) {
+					case "1": {
+						System.out.println( "Tipo de barco:" +
+								"\n		1.Velero" +
+								"\n		2.Embarcación deportiva" +
+								"\n		3.Yate de lujo"
+								);
+						System.out.print("Seleccione una función >> ");
+						opcion = new StringBuilder(entradaUsuario.nextLine());
+						if (Integer.parseInt(opcion.toString()) >= 1 && Integer.parseInt(opcion.toString()) <= 3){
+							throw new Exception("Opción incorrecta"); 
+						}
+						System.out.print("Inserte matricula: ");
+						matricula = new StringBuilder(entradaUsuario.nextLine());
+						System.out.print("Inserte medida de eslora: ");
+						eslora = Double.parseDouble(entradaUsuario.nextLine());
+						System.out.print("Inserte año de fabricación: ");
+						anoFabricacion = Integer.parseInt(entradaUsuario.nextLine());
+						switch (Integer.parseInt(opcion.toString())) {
+							case 1: {
+								System.out.print("Inserte número de mástiles: ");
+								numeroMastiles = Integer.parseInt(entradaUsuario.nextLine());
+								barco1 = new Velero(matricula.toString(),eslora,anoFabricacion,numeroMastiles);
+								break;
+							}
+							case 2: {
+								System.out.println("Inserte cv: ");
+								cv = Integer.parseInt(entradaUsuario.nextLine());
+								barco1 = new EmbarcacionDeportiva(matricula.toString(), eslora, anoFabricacion, cv);
+								break;
+							}
+							case 3: {
+								System.out.println("Inserte cv: ");
+								cv = Integer.parseInt(entradaUsuario.nextLine());
+								System.out.println("Inserte numero de camarotes: ");
+								numeroCamarotes = Integer.parseInt(entradaUsuario.nextLine());
+								barco1 = new YateDeLujo(matricula.toString(), eslora, anoFabricacion, cv, numeroCamarotes);
+								break;
+							}
+						}
+						break;
+					}
+					case "2": {
+						break;
+					}
+					case "3": {
+						break;
+					}
+					case "4": {
+						break;
+					}
+					case "5": {
+						break;
+					}
+					case "6": {
+						break;
+					}
+					case "7": {
+						break;
+					}
+					default: {
+						throw new Exception("Opción incorrecta"); 
+					}
 				}
-				case 2: {
-					System.out.println(alq0.precioAlquiler() + "€");
-					break;
-				}
-				case 3: {
-					System.out.println(alq1.toString());
-					break;
-				}
-				case 4: {
-					System.out.println(alq1.precioAlquiler() + "€");
-					break;
-				}
-				case 5: {
-					System.out.println(alq2.toString());
-					break;
-				}
-				case 6: {
-					System.out.println(alq2.precioAlquiler() + "€");
-					break;
-				}
-				default: {
-					System.out.println("Opción incorrecta");
-					break;
-				}
+			}
+			catch(Exception e) {
+				System.out.println(e);
 			}
 		}
 	}
