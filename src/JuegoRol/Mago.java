d9183f67a0c89e65c73d2a3e71d4ca153331b4c5
Nombre de usuario <txaxati2018@gmail.com>
2021-03-10 10:02:24 +0100

    Continuado con ejercicio 3

--- src/JuegoRol/Mago.java
+++ src/JuegoRol/Mago.java
@@ -1,18 +1,19 @@
 package JuegoRol;
 
 public class Mago extends Personaje {
+	private static final Integer NUMERO_MAXIMO_HECHIZOS = 4;
 	private static final Integer FUERZA_MAXIMA = 15;
 	private static final Integer INTELIGENCIA_MINIMA = 17;
 	private static final Integer VIDA_MAXIMA = 100;
+	
 	private String[] hechizos;
 
 	public Mago(String nombre, Raza raza, Integer fuerza, Integer inteligencia, Integer puntosVida,
 			Integer puntosActuales) throws Exception {
 		super(nombre, raza, fuerza, inteligencia, puntosVida, puntosActuales);
-//		if (){
-//			
-//		}
-		this.hechizos = new String[4];
+		this.setInteligencia(inteligencia);
+		this.setFuerza(fuerza);
+		this.hechizos = new String[NUMERO_MAXIMO_HECHIZOS];
 	}
 	@Override
 	public void setInteligencia(Integer inteligencia) throws Exception {
@@ -27,7 +28,20 @@ public class Mago extends Personaje {
 		if (inteligencia < 17) {
 			throw new Exception("Un mago no puede tener una inteligencia inferior a 17");
 		}
-		super.setInteligencia(inteligencia);
+		super.setFuerza(fuerza);
 	}
 	
+	public void aprendeHechizo(String hechizo) throws Exception {
+		Integer i = -1;
+		do {
+			i++;
+		}
+		while(hechizos[i] != null);
+		if (i != NUMERO_MAXIMO_HECHIZOS) {
+			hechizos[i] = hechizo;
+		}
+		else {
+			throw new Exception("Este mago sólo puede tener " + NUMERO_MAXIMO_HECHIZOS + " hechizos");
+		}
+	}
 }
